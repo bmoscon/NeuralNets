dbb392d2e97c822b224171d822da6c3d0689d79c
Bryant Moscon <bmoscon@gmail.com>
2013-09-12 20:17:23 -0400

    cleanup started

--- simple_neuralnet.py
+++ simple_neuralnet.py
@@ -42,76 +42,83 @@ import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
 
-# Make some training data
-n = 20
-X = np.linspace(0.,20.0,n).reshape((n,1))
-T = 0.2 + 0.05 * X + 0.4 * np.sin(X) + 0.05 * np.random.normal(size=(n,1))
-
-# Make some testing data
-Xtest = X + 0.1*np.random.normal(size=(n,1))
-Ttest = 0.2 + 0.05 * X + 0.4 * np.sin(Xtest) + 0.1 * np.random.normal(size=(n,1))
-
-nSamples = X.shape[0]
-nOutputs = T.shape[1]
-
-# Set parameters of neural network
-nHiddens = 20
-rhoh = .9
-rhoo = .1
-rh = rhoh / (nSamples*nOutputs)
-ro = rhoo / (nSamples*nOutputs)
-
-# Initialize weights to uniformly distributed values between small normally-distributed between -0.1 and 0.1
-V = 0.1*2*(np.random.uniform(size=(1+1,nHiddens))-0.5)
-W = 0.1*2*(np.random.uniform(size=(1+nHiddens,nOutputs))-0.5)
 
 # Add constant column of 1's
 def addOnes(A):
     return np.hstack((np.ones((A.shape[0],1)),A))
-X1 = addOnes(X)
-Xtest1 = addOnes(Xtest)
 
-# Take nReps steepest descent steps in gradient descent search in mean-squared-error function
-nReps = 200000
-# collect training and testing errors for plotting
-errorTrace = np.zeros((nReps,2))
-errorTrace[:] = np.nan
-for reps in range(nReps):
 
-    # Forward pass on training data
-    Z = np.tanh(np.dot( X1, V ))
-    Z1 = addOnes(Z)
-    Y = np.dot( Z1, W )
+def nnet(data_size, num_reps):
+    # Make some training data
+    X = np.linspace(0.0, 20.0, data_size).reshape((data_size, 1))
+    T = 0.2 + 0.05 * X + 0.4 * np.sin(X) + 0.05 * np.random.normal(size = (data_size, 1))
+    
+    # Make some testing data
+    Xtest = X + 0.1 * np.random.normal(size = (data_size, 1))
+    Ttest = 0.2 + 0.05 * X + 0.4 * np.sin(Xtest) + 0.1 * np.random.normal(size = (data_size, 1))
+    
+    nSamples = X.shape[0]
+    nOutputs = T.shape[1]
+    
+    # Set parameters of neural network
+    nHiddens = 20
+    rhoh = .9
+    rhoo = .1
+    rh = rhoh / (nSamples * nOutputs)
+    ro = rhoo / (nSamples * nOutputs)
+    
+    # Initialize weights to uniformly distributed values
+    V = 0.1 * 2 *(np.random.uniform(size = (1 + 1,nHiddens)) - 0.5)
+    W = 0.1 * 2 *(np.random.uniform(size = (1 + nHiddens, nOutputs)) - 0.5)
 
-    # Error in output
-    error = Y - T
-    nan = np.isnan(error)
-    if nan[0]:
-        print "NAN!"
 
-    # Backward pass - the backpropagation and weight update steps
-    V = V - rh * np.dot( X1.T, np.dot( error, W[1:,:].T) * (1-Z**2))
-    W = W - ro * np.dot( Z1.T, error)
+    X1 = addOnes(X)
+    Xtest1 = addOnes(Xtest)
+    
+    # Collect training and testing errors for plotting
+    errorTrace = np.zeros((num_reps, 2))
+    errorTrace[:] = np.nan
+    
+    # Take nReps steepest descent steps in gradient descent search in mean-squared-error function
+    for reps in range(num_reps):
 
-    # error traces for plotting
-    errorTrace[reps,0] = sqrt(np.mean((error**2).flat))
-    Ytest = np.dot(addOnes(np.tanh(np.dot(Xtest1,V))), W)
-    errorTrace[reps,1] = sqrt(np.mean(((Ytest-Ttest)**2).flat))
-
-    # Every so often update the graphs
-    if reps % (nReps/500) == 0:
-        plt.ion()
-        plt.figure(1)
-        plt.clf()
-        plt.subplot(3,1,1)
-        plt.plot(np.arange(nReps),errorTrace)
-
-        plt.subplot(3,1,2)
-        plt.plot(X,T,'o-',Xtest,Ttest,'o-',Xtest,Ytest,'o-')
-        plt.legend(('Training','Testing','Model'),'lower right')
+        # Forward pass on training data
+        Z = np.tanh(np.dot(X1, V))
+        Z1 = addOnes(Z)
+        Y = np.dot(Z1, W)
         
-        plt.subplot(3,1,3)
-        plt.plot(X,Z)
-        plt.draw()
+        # Error in output
+        error = Y - T
+        nan = np.isnan(error)
+        if nan[0]:
+            print "NAN!"
 
-plt.show()
+        # Backward pass - the backpropagation and weight update steps
+        V = V - rh * np.dot(X1.T, np.dot(error, W[1:,:].T) * (1 - Z**2))
+        W = W - ro * np.dot(Z1.T, error)
+
+        # error traces for plotting
+        errorTrace[reps, 0] = sqrt(np.mean((error**2).flat))
+        Ytest = np.dot(addOnes(np.tanh(np.dot(Xtest1, V))), W)
+        errorTrace[reps, 1] = sqrt(np.mean(((Ytest - Ttest)**2).flat))
+        
+        # Every so often update the graphs
+        if reps & 1023 == 0:
+            plt.ion()
+            plt.figure(1)
+            plt.clf()
+            plt.subplot(3, 1, 1)
+            plt.plot(np.arange(num_reps), errorTrace)
+            
+            plt.subplot(3, 1, 2)
+            plt.plot(X, T, 'o-', Xtest, Ttest, 'o-', Xtest, Ytest, 'o-')
+            plt.legend(('Training', 'Testing', 'Model'), 'lower right')
+            
+            plt.subplot(3, 1, 3)
+            plt.plot(X, Z)
+            plt.draw()
+            
+
+
+if __name__ == "__main__":
+    nnet(100, 100000)
